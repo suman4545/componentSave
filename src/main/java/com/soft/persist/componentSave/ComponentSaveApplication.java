412252c7751930a354583b7b6eaf202936c33fcf
suman4545 <smn.bsws@gmail.com>
2018-08-25 22:01:46 +0530

    some init msg

---
+++ src/main/java/com/soft/persist/componentSave/ComponentSaveApplication.java
@@ -0,0 +1,12 @@
+package com.soft.persist.componentSave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentSaveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/soft/persist/componentSave/ComponentSaveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soft.persist.componentSave;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComponentSaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
